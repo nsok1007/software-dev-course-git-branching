6b2d2592f323b93f84c3a71c4bd28ae12c57c6f6
nsok1007 <nsok1007@gmail.com>
2026-05-18 14:29:22 -0500

    updated comment in feature-conflict branch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Nik - 5/18/26
-// This application prints "Hello, World!" and the numbers 0 to 9. 
+// This application prints "Hello, World!" and the numbers 0 to 9, along with their squares. 
 console.log("Hello world!");
 
 for (let i = 0; i < 10; i++) {
